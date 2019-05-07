77b6c4a82448cb1b0c9a41cba41cf7c7db97edaf
bui.ngoc.hoang@framgia.com <buihoangbg01@gmail.com>
2019-05-07 09:47:49 +0700

    Single-handing áºerror request

--- app/src/main/java/com/example/hdev/mvvm_demo/ui/main/UserViewModel.kt
+++ app/src/main/java/com/example/hdev/mvvm_demo/ui/main/UserViewModel.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.hdev.mvvm_demo.data.model.User
 import com.example.hdev.mvvm_demo.data.repository.UserRepository
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.functions.Function3
 import io.reactivex.schedulers.Schedulers
 
 class UserViewModel(private val repository: UserRepository) : ViewModel() {
@@ -20,7 +22,24 @@ class UserViewModel(private val repository: UserRepository) : ViewModel() {
     val userViewModel: LiveData<List<User>>
         get() = _userViewModel
 
+    class UserDataWrap(val users: List<User>, val clazz: List<User>, val town: List<User>)
+
     fun getUsers() {
+        val usersSingle = repository.getUsers().onErrorReturn { ArrayList() }
+        val clazzSingle = repository.getUsers().onErrorReturn { ArrayList() }
+        val hometownSingle = repository.getUsers().onErrorReturn { ArrayList() }
+        val disposable1 =
+            Single.zip<List<User>, List<User>, List<User>, UserDataWrap>(usersSingle, clazzSingle, hometownSingle,
+                Function3 { users1, users2, users3 ->
+                    return@Function3 UserDataWrap(users1, users2, users3)
+                })
+                .subscribe({ userData ->
+                    userData.users
+                    userData.clazz
+                    userData.town
+                }, { throwable ->
+                })
+
         val disposable = repository.getUsers().subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
@@ -31,9 +50,10 @@ class UserViewModel(private val repository: UserRepository) : ViewModel() {
             })
 
         compositeDisposable.add(disposable)
+        compositeDisposable.add(disposable1)
     }
 
-    fun onDestroy(){
-        compositeDisposable.clear()
+    fun onDestroy() {
+        compositeDisposable.dispose()
     }
 }
